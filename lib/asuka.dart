15542dd69cf28a8a6519e480a335e2b70d278728
gustavo <gustavorw03@gmail.com>
2022-08-05 18:00:34 -0300

    add private constructor in Asuka class

--- lib/asuka.dart
+++ lib/asuka.dart
@@ -2,3 +2,4 @@ library asuka;
 
 export 'src/asuka_singleton.dart';
 export 'snackbars/asuka_snack_bar.dart';
+export 'package:asuka/src/asuka_singleton_deprecated.dart';
--- test/asuka_test.dart
+++ test/asuka_test.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:asuka/asuka.dart' as asuka;
+import 'package:asuka/asuka.dart';
 
 void main() {
   test('erro when not called build method', () {
-    expect(() => asuka.showSnackBar(SnackBar(content: Container())), throwsAssertionError);
-    expect(() => asuka.hideCurrentSnackBar(), throwsAssertionError);
-    expect(() => asuka.removeCurrentSnackBar(), throwsAssertionError);
-    expect(() => asuka.showBottomSheet((e) => Container()), throwsAssertionError);
-    expect(() => asuka.showDialog(builder: (e) => Container()), throwsAssertionError);
+    expect(() => Asuka.showSnackBar(SnackBar(content: Container())),
+        throwsAssertionError);
+    expect(() => Asuka.hideCurrentSnackBar(), throwsAssertionError);
+    expect(() => Asuka.removeCurrentSnackBar(), throwsAssertionError);
+    expect(
+        () => Asuka.showBottomSheet((e) => Container()), throwsAssertionError);
+    expect(() => Asuka.showDialog(builder: (e) => Container()),
+        throwsAssertionError);
   });
 }
