66eafdfeb83532edbc89a11f1fb21c7dcf368f06
Jennifer Sarah <sjennifersarah@gmail.com>
2022-08-26 16:40:22 -0300

    doc: documentação alterada para documentação dart (#23)
    * Documentação alterada para documentação dart
    
    * Adição documentação showdialog e etc. Correção second page
    
    * Padronização example

--- example/lib/src/second/second_page.dart
+++ example/lib/src/second/second_page.dart
@@ -19,68 +19,70 @@ class _SecondPageState extends State<SecondPage> {
           title: const Text("Asuka"),
         ),
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Hero(
-                tag: "HeroTag",
-                child: Container(
-                  height: 100,
-                  color: Colors.red,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Hero(
+                  tag: "HeroTag",
+                  child: Container(
+                    height: 100,
+                    color: Colors.red,
+                  ),
                 ),
-              ),
-              const TextField(),
-              ElevatedButton(
-                  child: const Text('Back to Home'),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  }),
-              ElevatedButton(
-                  child: const Text('Back to Home'),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  }),
-              ElevatedButton(
-                  child: const Text('Back to Home'),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  }),
-              ElevatedButton(
-                  child: const Text('Back to Home'),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  }),
-              ElevatedButton(
-                  child: const Text('Back to Home'),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  }),
-              ElevatedButton(
-                  child: const Text('Back to Home'),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  }),
-              ElevatedButton(
-                  child: const Text('Back to Home'),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  }),
-              ElevatedButton(
-                  child: const Text('Back to Home'),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  }),
-              ElevatedButton(
-                  child: const Text('Back to Home'),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  }),
-              ElevatedButton(
-                  child: const Text('Back to Home'),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  }),
-            ],
+                const TextField(),
+                ElevatedButton(
+                    child: const Text('Back to Home'),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    }),
+                ElevatedButton(
+                    child: const Text('Back to Home'),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    }),
+                ElevatedButton(
+                    child: const Text('Back to Home'),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    }),
+                ElevatedButton(
+                    child: const Text('Back to Home'),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    }),
+                ElevatedButton(
+                    child: const Text('Back to Home'),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    }),
+                ElevatedButton(
+                    child: const Text('Back to Home'),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    }),
+                ElevatedButton(
+                    child: const Text('Back to Home'),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    }),
+                ElevatedButton(
+                    child: const Text('Back to Home'),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    }),
+                ElevatedButton(
+                    child: const Text('Back to Home'),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    }),
+                ElevatedButton(
+                    child: const Text('Back to Home'),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    }),
+              ],
+            ),
           ),
         ));
   }
--- lib/src/asuka_singleton.dart
+++ lib/src/asuka_singleton.dart
@@ -57,21 +57,35 @@ class _Asuka {
         .insertAll(entries, below: below, above: above);
   }
 
-  /// Shows a [SnackBar] at the bottom of the scaffold.
-  ///
-  /// A scaffold can show at most one snack bar at a time. If this function is
-  /// called while another snack bar is already visible, the given snack bar
-  /// will be added to a queue and displayed after the earlier snack bars have
-  /// closed.
-  ///
-  /// To control how long a [SnackBar] remains visible, use [SnackBar.duration].
-  ///
-  /// To remove the [SnackBar] with an exit animation, use [hideCurrentSnackBar]
-  /// or call [ScaffoldFeatureController.close] on the returned
-  /// [ScaffoldFeatureController]. To remove a [SnackBar] suddenly (without an
-  /// animation), use [removeCurrentSnackBar].
-  ///
-  /// See [Scaffold.of] for information about how to obtain the [ScaffoldState].
+///Asuka showSnackBar
+///Asuka [showSnackBar] was implemented with the purpose of supplying the user with a complete [snackbar].
+///
+///Example:
+///
+///Controller:
+///
+///```
+///void onClickSnackbar() {
+///    asuka.showSnackBar(
+///     const SnackBar(
+///        content: Text('New Snackbar'),
+///      ),
+///    );
+/// }
+///```
+///
+///Page:
+///
+///```
+///ElevatedButton(
+///                  style: ElevatedButton.styleFrom(primary: Colors.red[300]),
+///                  onPressed: controller.onClickSnackbar,
+///                  child: const Text('Custom SnackBar'),
+///                ),
+///```
+///
+///This code generates an **ElevatedButton**, when pressed, it calls **controller.onClickSnackbar**
+///
   material.ScaffoldFeatureController<material.SnackBar,
           material.SnackBarClosedReason>
       showSnackBar(material.SnackBar snackbar, {bool callback = false}) {
@@ -109,32 +123,57 @@ class _Asuka {
         .hideCurrentSnackBar(reason: reason);
   }
 
-  /// Shows a material design bottom sheet in the nearest [Scaffold]. To show
-  /// a persistent bottom sheet, use the [Scaffold.bottomSheet].
-  ///
-  /// Returns a controller that can be used to close and otherwise manipulate the
-  /// bottom sheet.
-  ///
-  /// To rebuild the bottom sheet (e.g. if it is stateful), call
-  /// [PersistentBottomSheetController.setState] on the controller returned by
-  /// this method.
-  ///
-  /// The new bottom sheet becomes a [LocalHistoryEntry] for the enclosing
-  /// [ModalRoute] and a back button is added to the app bar of the [Scaffold]
-  /// that closes the bottom sheet.
-  ///
-  /// To create a persistent bottom sheet that is not a [LocalHistoryEntry] and
-  /// does not add a back button to the enclosing Scaffold's app bar, use the
-  /// [Scaffold.bottomSheet] constructor parameter.
-  ///
-  /// A persistent bottom sheet shows information that supplements the primary
-  /// content of the app. A persistent bottom sheet remains visible even when
-  /// the user interacts with other parts of the app.
-  ///
-  /// A closely related widget is a modal bottom sheet, which is an alternative
-  /// to a menu or a dialog and prevents the user from interacting with the rest
-  /// of the app. Modal bottom sheets can be created and displayed with the
-  /// [showModalBottomSheet] function.
+  
+///Asuka showBottomSheet
+///
+///Asuka [showBottomSheet] was implemented with the purpose of supplying the user with a complete [bottomsheet].
+///
+///Example:
+///
+///Controller:
+///
+///```
+///asuka.showBottomSheet((context) {
+///      return Material(
+///        elevation: 7,
+///        child: SizedBox(
+///          height: MediaQuery.of(context).size.height / 2,
+///          child: ListView(
+///            children: [
+///              const ListTile(
+///                title: Text('1'),
+///              ),
+///              const ListTile(
+///                title: Text('2'),
+///              ),
+///              const ListTile(
+///                title: Text('3'),
+///              ),
+///              ListTile(
+///                title: const Text('Cancel'),
+///                onTap: () => Navigator.pop(context),
+///              ),
+///            ],
+///         ),
+///        ),
+///      );
+///   });
+///```
+///
+///Page:
+///
+///```
+///ElevatedButton(
+///                 style: ElevatedButton.styleFrom(primary: Colors.red[300]),
+///                  onPressed: () {
+///                    controller.onClickBottomSheet();
+///                  },
+///                  child: const Text('Bottom Sheet'),
+///                ),
+///```
+///
+///This code generates an **ElevatedButton**, when pressed, it calls **controller.onClickBottomSheet()**
+
   material.PersistentBottomSheetController<T> showBottomSheet<T>(
       Widget Function(BuildContext) builder,
       {Color? backgroundColor,
@@ -153,52 +192,62 @@ class _Asuka {
     );
   }
 
-  /// Shows a modal material design bottom sheet.
-  ///
-  /// A modal bottom sheet is an alternative to a menu or a dialog and prevents
-  /// the user from interacting with the rest of the app.
-  ///
-  /// A closely related widget is a persistent bottom sheet, which shows
-  /// information that supplements the primary content of the app without
-  /// preventing the use from interacting with the app. Persistent bottom sheets
-  /// can be created and displayed with the [showBottomSheet] function or the
-  /// [ScaffoldState.showBottomSheet] method.
-  ///
-  /// The `context` argument is used to look up the [Navigator] and [Theme] for
-  /// the bottom sheet. It is only used when the method is called. Its
-  /// corresponding widget can be safely removed from the tree before the bottom
-  /// sheet is closed.
-  ///
-  /// The `isScrollControlled` parameter specifies whether this is a route for
-  /// a bottom sheet that will utilize [DraggableScrollableSheet]. If you wish
-  /// to have a bottom sheet that has a scrollable child such as a [ListView] or
-  /// a [GridView] and have the bottom sheet be draggable, you should set this
-  /// parameter to true.
-  ///
-  /// The `useRootNavigator` parameter ensures that the root navigator is used to
-  /// display the [BottomSheet] when set to `true`. This is useful in the case
-  /// that a modal [BottomSheet] needs to be displayed above all other content
-  /// but the caller is inside another [Navigator].
-  ///
-  /// The [isDismissible] parameter specifies whether the bottom sheet will be
-  /// dismissed when user taps on the scrim.
-  ///
-  /// The [enableDrag] parameter specifies whether the bottom sheet can be
-  /// dragged up and down and dismissed by swiping downwards.
-  ///
-  /// The optional [backgroundColor], [elevation], [shape], [clipBehavior] and [transitionAnimationController]
-  /// parameters can be passed in to customize the appearance and behavior of
-  /// modal bottom sheets.
-  ///
-  /// The [transitionAnimationController] controls the bottom sheet's entrance and
-  /// exit animations if provided.
-  ///
-  /// The optional `routeSettings` parameter sets the [RouteSettings] of the modal bottom sheet
-  /// sheet. This is particularly useful in the case that a user wants to observe
-  /// [PopupRoute]s within a [NavigatorObserver].
-  ///
-  /// Returns a `Future` that resolves to the value (if any) that was passed to
-  /// [Navigator.pop] when the modal bottom sheet was closed.
+  
+///Asuka showModalBottomSheet
+///
+///Asuka showModalBottomSheet was implemented with the purpose of supplying the user with a complete modal bottom sheet.
+///
+///Example:
+///
+///Controller:
+///
+///```
+///asuka.showModalBottomSheet(
+///      builder: (context) => Material(
+///        borderRadius: const BorderRadius.only(
+///          topLeft: Radius.circular(16),
+///          topRight: Radius.circular(16),
+///        ),
+///        elevation: 7,
+///        child: SizedBox(
+///          height: MediaQuery.of(context).size.height / 2,
+///          child: ListView(
+///            children: [
+///              const ListTile(
+///                title: Text('Modal 1'),
+///              ),
+///              const ListTile(
+///                title: Text('Modal 2'),
+///              ),
+///              const ListTile(
+///                title: Text('Modal 3'),
+///              ),
+///              ListTile(
+///                title: const Text('Cancel'),
+///                onTap: () => Navigator.pop(context),
+///              ),
+///            ],
+///          ),
+///        ),
+///      ),
+///      backgroundColor: Colors.transparent,
+///    );
+///```
+///
+///Page:
+///
+///```
+///ElevatedButton(
+///                  style: ElevatedButton.styleFrom(primary: Colors.red[300]),
+///                  onPressed: () {
+///                    controller.onClickModalBottomSheet();
+///                  },
+///                  child: const Text('Modal Bottom Sheet'),
+///               ),
+///```
+///
+///This code generates an **ElevatedButton**, when pressed, it calls **controller.onClickModalBottomSheet();**
+
   Future<T?> showModalBottomSheet<T>({
     required WidgetBuilder builder,
     Color? backgroundColor,
@@ -232,59 +281,46 @@ class _Asuka {
     );
   }
 
-  /// Displays a Material dialog above the current contents of the app, with
-  /// Material entrance and exit animations, modal barrier color, and modal
-  /// barrier behavior (dialog is dismissible with a tap on the barrier).
+  /// Asuka showDialog
   ///
-  /// This function takes a `builder` which typically builds a [Dialog] widget.
-  /// Content below the dialog is dimmed with a [ModalBarrier]. The widget
-  /// returned by the `builder` does not share a context with the location that
-  /// `showDialog` is originally called from. Use a [StatefulBuilder] or a
-  /// custom [StatefulWidget] if the dialog needs to update dynamically.
+  /// Asuka [showDialog] was implemented with the purpose of supplying the user with a [dialog].
   ///
-  /// The `child` argument is deprecated, and should be replaced with `builder`.
+  ///Example:
   ///
-  /// The `context` argument is used to look up the [Navigator] and [Theme] for
-  /// the dialog. It is only used when the method is called. Its corresponding
-  /// widget can be safely removed from the tree before the dialog is closed.
+  /// Controller:
+  ///```
+  /// asuka.showDialog(
+  ///       builder: (context) => AlertDialog(
+  ///         title: const Text('Dialog'),
+  ///         content: const Text('This is a custom Dialog '),
+  ///         actions: [
+  ///           TextButton(
+  ///             onPressed: () {
+  ///               Navigator.pop(context);
+  ///             },
+  ///             child: const Text('Cancel'),
+  ///           ),
+  ///           TextButton(
+  ///             onPressed: () {
+  ///               Navigator.pop(context);
+  ///             },
+  ///             child: const Text('Ok'),
+  ///           ),
+  ///         ],
+  ///       ),
+  ///     );
+  ///```
+  ///Page:
   ///
-  /// The `barrierDismissible` argument is used to indicate whether tapping on the
-  /// barrier will dismiss the dialog. It is `true` by default and can not be `null`.
-  ///
-  /// The `barrierColor` argument is used to specify the color of the modal
-  /// barrier that darkens everything the dialog. If `null` the default color
-  /// `Colors.black54` is used.
-  ///
-  /// The `useSafeArea` argument is used to indicate if the dialog should only
-  /// display in 'safe' areas of the screen not used by the operating system
-  /// (see [SafeArea] for more details). It is `true` by default which will mean
-  /// the dialog will not overlap operating system areas. If it is set to `false`
-  /// the dialog will only be constrained by the screen size. It can not be 'null`.
-//
-  /// The `useRootNavigator` argument is used to determine whether to push the
-  /// dialog to the [Navigator] furthest from or nearest to the given `context`.
-  /// By default, `useRootNavigator` is `true` and the dialog route created by
-  /// this method is pushed to the root navigator. It can not be `null`.
-  ///
-  /// The `routeSettings` argument is passed to [showGeneralDialog],
-  /// see [RouteSettings] for details.
-  ///
-  /// If the application has multiple [Navigator] objects, it may be necessary to
-  /// call `Navigator.of(context, rootNavigator: true).pop(result)` to close the
-  /// dialog rather than just `Navigator.pop(context, result)`.
-  ///
-  /// Returns a [Future] that resolves to the value (if any) that was passed to
-  /// [Navigator.pop] when the dialog was closed.
-  ///
-  /// See also:
-  ///
-  ///  * [AlertDialog], for dialogs that have a row of buttons below a body.
-  ///  * [SimpleDialog], which handles the scrolling of the contents and does
-  ///    not show buttons below its body.
-  ///  * [Dialog], on which [SimpleDialog] and [AlertDialog] are based.
-  ///  * [showCupertinoDialog], which displays an iOS-style dialog.
-  ///  * [showGeneralDialog], which allows for customization of the dialog popup.
-  ///  * <https://material.io/design/components/dialogs.html>
+  ///```
+  ///ElevatedButton(
+  /// style: ElevatedButton.styleFrom(primary: Colors.red[300]),
+  /// onPressed: controller.onClickDialog,
+  /// child: const Text('Custom Dialog'),
+  ///  ),
+  ///```
+  ///This code generates an **ElevatedButton**, when pressed, it calls **controller.onClickDialog**
+
   Future<T?> showDialog<T>(
       {required WidgetBuilder builder,
       bool barrierDismissible = true,
@@ -308,6 +344,18 @@ class _Asuka {
   /// return MaterialApp(
   ///         builder: asuka.builder,
   ///         ...
+  /// 
+  /// Example:
+  /// ```
+/// import 'package:asuka/asuka.dart' as asuka;
+///
+///  MaterialApp(
+///      builder: asuka.builder,
+///      navigatorObservers: [
+///         asuka.asukaHeroController //if u don`t add this Hero will not work
+///      ],
+///  );
+/// ```
 
   Widget builder(BuildContext context, Widget? child) {
     return Navigator(
