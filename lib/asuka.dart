82e0b98497d19d42e974d8252b31d13fde8e4ffb
Marcus Rohden <marcus.rohden@gmail.com>
2024-02-16 15:47:07 +1000

    Removing <T> parameter from PersistentBottomSheetController. Fix Flutter version 3.19

--- lib/asuka.dart
+++ lib/asuka.dart
@@ -1,7 +1,5 @@
 library asuka;
 
-export 'package:asuka/src/asuka_singleton_deprecated.dart';
-
 export 'material_banner/material_banner.dart';
 export 'snackbars/asuka_snack_bar.dart';
 export 'src/asuka_singleton.dart';
